319ae2dc5927f99b18f2870d84e2274c61c2b6a3
developerdizzle <david.bohner@gmail.com>
2015-02-19 03:10:24 +0000

    Adds tests for host and protocol

--- test/node-force-domainTests.js
+++ test/node-force-domainTests.js
@@ -123,6 +123,105 @@ suite('node-force-domain', function () {
     });
   });
 
+  suite('hostname and protocol', function () {
+    var app = express();
+
+    app.use(forceDomain({
+      protocol: 'https',
+      hostname: 'www.example.com'
+    }));
+
+    app.get('/', function (req, res) {
+      res.send(200);
+    });
+
+    var appHttps = https.createServer({
+      key: fs.readFileSync(path.join(__dirname, 'keys', 'privateKey.pem')),
+      cert: fs.readFileSync(path.join(__dirname, 'keys', 'certificate.pem')),
+      ca: [ fs.readFileSync(path.join(__dirname, 'keys', 'caCertificate.pem')) ]
+    }, app);
+
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+    
+    test('does not redirect on localhost.', function (done) {
+      request(app)
+        .get('/')
+        .set('host', 'localhost:3000')
+        .end(function (err, res) {
+          assert.that(err, is.null());
+          assert.that(res.statusCode, is.equalTo(200));
+          res.resume();
+          done();
+        });
+    });
+
+    test('redirects on correct host and port, but other protocol.', function (done) {
+      request(app)
+        .get('/')
+        .set('protocol', 'http')
+        .set('host', 'www.example.com:4000')
+        .end(function (err, res) {
+          assert.that(err, is.null());
+          assert.that(res.statusCode, is.equalTo(307));
+          res.resume();
+          done();
+        });
+    });
+
+    test('redirects temporarily on correct host, but other protocol.', function (done) {
+      request(app)
+        .get('/')
+        .set('host', 'www.example.com')
+        .end(function (err, res) {
+          assert.that(err, is.null());
+          assert.that(res.statusCode, is.equalTo(307));
+          assert.that(res.header.location, is.equalTo('https://www.example.com/'));
+          res.resume();
+          done();
+        });
+    });
+
+    test('redirects temporarily on any other host, other protocol.', function (done) {
+      request(app)
+        .get('/')
+        .set('host', 'www.thenativeweb.io')
+        .end(function (err, res) {
+          assert.that(err, is.null());
+          assert.that(res.statusCode, is.equalTo(307));
+          assert.that(res.header.location, is.equalTo('https://www.example.com/'));
+          res.resume();
+          done();
+        });
+    });
+
+    test('redirects temporarily on any other host, same protocol.', function (done) {
+      request(appHttps)
+        .get('/')
+        .set('host', 'www.thenativeweb.io')
+        .end(function (err, res) {
+          assert.that(err, is.null());
+          assert.that(res.statusCode, is.equalTo(307));
+          assert.that(res.header.location, is.equalTo('https://www.example.com/'));
+          res.resume();
+          done();
+        });
+    });
+    
+    test('does not redirect on correct protocol and host.', function (done) {
+      request(appHttps)
+        .get('/')
+        .set('protocol', 'https')
+        .set('host', 'www.example.com')
+        .end(function (err, res) {
+          assert.that(err, is.null());
+          assert.that(res.statusCode, is.equalTo(200));
+          res.resume();
+          done();
+        });
+    });    
+  });
+
+
   suite('hostname, port and protocol', function () {
     var app = express();
 
