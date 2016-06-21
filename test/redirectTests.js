54405f46860750f989fc6ec614172e7d19545669
huseyin uslu <shalafiraistlin@gmail.com>
2016-06-21 20:16:18 +0300

    Fixed indentations.

--- test/redirectTests.js
+++ test/redirectTests.js
@@ -24,12 +24,12 @@ suite('redirect', function () {
       done();
     });
 
-	test('for https localhost:3000.', function(done) {
-		assert.that(redirect('https', 'localhost:3000', '/foo/bar', {
-			hostname: 'www.thenativeweb.io'
-		})).is.null();
-		done();
-	});
+    test('for https localhost:3000.', function (done) {
+      assert.that(redirect('https', 'localhost:3000', '/foo/bar', {
+        hostname: 'www.thenativeweb.io'
+      })).is.null();
+      done();
+    });
 
     test('for the forced domain with the correct port.', function (done) {
       assert.that(redirect('http', 'www.thenativeweb.io:4000', '/foo/bar', {
@@ -91,37 +91,37 @@ suite('redirect', function () {
       done();
     });
 
-	test('for another domain with the correct protocol.', function (done) {
-		assert.that(redirect('http', 'thenativeweb.io:4000', '/foo/bar', {
-			hostname: 'www.thenativeweb.io',
-			protocol: 'http'
-		})).is.equalTo({
-			type: 'permanent',
-			url: 'http://www.thenativeweb.io:4000/foo/bar'
-		});
-		done();
-	});
+    test('for another domain with the correct protocol.', function (done) {
+      assert.that(redirect('http', 'thenativeweb.io:4000', '/foo/bar', {
+        hostname: 'www.thenativeweb.io',
+        protocol: 'http'
+      })).is.equalTo({
+        type: 'permanent',
+        url: 'http://www.thenativeweb.io:4000/foo/bar'
+      });
+      done();
+    });
 
-	test('for another domain with the another protocol.', function (done) {
-		assert.that(redirect('http', 'thenativeweb.io:4000', '/foo/bar', {
-			hostname: 'www.thenativeweb.io',
-			protocol: 'https'
-		})).is.equalTo({
-			type: 'permanent',
-			url: 'https://www.thenativeweb.io:4000/foo/bar'
-		});
-		done();
-	});
+    test('for another domain with the another protocol.', function (done) {
+      assert.that(redirect('http', 'thenativeweb.io:4000', '/foo/bar', {
+        hostname: 'www.thenativeweb.io',
+        protocol: 'https'
+      })).is.equalTo({
+        type: 'permanent',
+        url: 'https://www.thenativeweb.io:4000/foo/bar'
+      });
+      done();
+    });
 
-	test('for another domain without changing protocol.', function (done) {
-		assert.that(redirect('https', 'thenativeweb.io:4000', '/foo/bar', {
-			hostname: 'www.thenativeweb.io',
-		})).is.equalTo({
-			type: 'permanent',
-			url: 'https://www.thenativeweb.io:4000/foo/bar'
-		});
-		done();
-	});
+    test('for another domain without changing protocol.', function (done) {
+      assert.that(redirect('https', 'thenativeweb.io:4000', '/foo/bar', {
+        hostname: 'www.thenativeweb.io'
+      })).is.equalTo({
+        type: 'permanent',
+        url: 'https://www.thenativeweb.io:4000/foo/bar'
+      });
+      done();
+    });
   });
 
   suite('returns a temporary redirect.', function () {
@@ -161,40 +161,40 @@ suite('redirect', function () {
       done();
     });
 
-	test('for another domain with the correct protocol.', function (done) {
-		assert.that(redirect('http', 'thenativeweb.io:4000', '/foo/bar', {
-			hostname: 'www.thenativeweb.io',
-			protocol: 'http',
-			type: 'temporary'
-		})).is.equalTo({
-			type: 'temporary',
-			url: 'http://www.thenativeweb.io:4000/foo/bar'
-		});
-		done();
-	});
+    test('for another domain with the correct protocol.', function (done) {
+      assert.that(redirect('http', 'thenativeweb.io:4000', '/foo/bar', {
+        hostname: 'www.thenativeweb.io',
+        protocol: 'http',
+        type: 'temporary'
+      })).is.equalTo({
+        type: 'temporary',
+        url: 'http://www.thenativeweb.io:4000/foo/bar'
+      });
+      done();
+    });
 
-	test('for another domain with the another protocol.', function (done) {
-		assert.that(redirect('http', 'thenativeweb.io:4000', '/foo/bar', {
-			hostname: 'www.thenativeweb.io',
-			protocol: 'https',
-			type: 'temporary'
-		})).is.equalTo({
-			type: 'temporary',
-			url: 'https://www.thenativeweb.io:4000/foo/bar'
-		});
-		done();
-	});
+    test('for another domain with the another protocol.', function (done) {
+      assert.that(redirect('http', 'thenativeweb.io:4000', '/foo/bar', {
+        hostname: 'www.thenativeweb.io',
+        protocol: 'https',
+        type: 'temporary'
+      })).is.equalTo({
+        type: 'temporary',
+        url: 'https://www.thenativeweb.io:4000/foo/bar'
+      });
+      done();
+    });
 
-	test('for another domain without changing protocol.', function (done) {
-		assert.that(redirect('https', 'thenativeweb.io:4000', '/foo/bar', {
-			hostname: 'www.thenativeweb.io',
-			type: 'temporary'
-		})).is.equalTo({
-			type: 'temporary',
-			url: 'https://www.thenativeweb.io:4000/foo/bar'
-		});
-		done();
-	});
+    test('for another domain without changing protocol.', function (done) {
+      assert.that(redirect('https', 'thenativeweb.io:4000', '/foo/bar', {
+        hostname: 'www.thenativeweb.io',
+        type: 'temporary'
+      })).is.equalTo({
+        type: 'temporary',
+        url: 'https://www.thenativeweb.io:4000/foo/bar'
+      });
+      done();
+    });
 
     test('when non-www is preferred.', function (done) {
       assert.that(redirect('https', 'www.thenativeweb.io', '/foo/bar', {
