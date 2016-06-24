418b9fabbd118a9d49c6e3b426ae6ae9dbafdf7e
Hüseyin Uslu <shalafiraistlin@gmail.com>
2016-06-24 15:42:57 +0300

    Actual fix for #7 prevent https from being redirected to http. (#14)
    * Actual fix for #7 prevent https from being redirected to http.
    
    * fix indentation.
    
    * Update redirect.js
    
    * added protocol related tests to redirectTests.js.
    fixed incorrect suite names in redirectTests.js.
    
    * Fixed indentations.

--- test/redirectTests.js
+++ test/redirectTests.js
@@ -24,6 +24,13 @@ suite('redirect', function () {
       done();
     });
 
+    test('for https localhost:3000.', function (done) {
+      assert.that(redirect('https', 'localhost:3000', '/foo/bar', {
+        hostname: 'www.thenativeweb.io'
+      })).is.null();
+      done();
+    });
+
     test('for the forced domain with the correct port.', function (done) {
       assert.that(redirect('http', 'www.thenativeweb.io:4000', '/foo/bar', {
         hostname: 'www.thenativeweb.io',
@@ -50,7 +57,7 @@ suite('redirect', function () {
     });
   });
 
-  suite('returns a temporary redirect', function () {
+  suite('returns a permanent redirect', function () {
     test('for the forced domain with another port.', function (done) {
       assert.that(redirect('http', 'www.thenativeweb.io:3000', '/foo/bar', {
         hostname: 'www.thenativeweb.io',
@@ -80,12 +87,44 @@ suite('redirect', function () {
       })).is.equalTo({
         type: 'permanent',
         url: 'http://www.thenativeweb.io:4000/foo/bar'
+      });
+      done();
+    });
+
+    test('for another domain with the correct protocol.', function (done) {
+      assert.that(redirect('http', 'thenativeweb.io:4000', '/foo/bar', {
+        hostname: 'www.thenativeweb.io',
+        protocol: 'http'
+      })).is.equalTo({
+        type: 'permanent',
+        url: 'http://www.thenativeweb.io:4000/foo/bar'
+      });
+      done();
+    });
+
+    test('for another domain with the another protocol.', function (done) {
+      assert.that(redirect('http', 'thenativeweb.io:4000', '/foo/bar', {
+        hostname: 'www.thenativeweb.io',
+        protocol: 'https'
+      })).is.equalTo({
+        type: 'permanent',
+        url: 'https://www.thenativeweb.io:4000/foo/bar'
+      });
+      done();
+    });
+
+    test('for another domain without changing protocol.', function (done) {
+      assert.that(redirect('https', 'thenativeweb.io:4000', '/foo/bar', {
+        hostname: 'www.thenativeweb.io'
+      })).is.equalTo({
+        type: 'permanent',
+        url: 'https://www.thenativeweb.io:4000/foo/bar'
       });
       done();
     });
   });
 
-  suite('returns a permanent redirect.', function () {
+  suite('returns a temporary redirect.', function () {
     test('for the forced domain with another port.', function (done) {
       assert.that(redirect('http', 'www.thenativeweb.io:3000', '/foo/bar', {
         hostname: 'www.thenativeweb.io',
@@ -118,6 +157,41 @@ suite('redirect', function () {
       })).is.equalTo({
         type: 'temporary',
         url: 'http://www.thenativeweb.io:4000/foo/bar'
+      });
+      done();
+    });
+
+    test('for another domain with the correct protocol.', function (done) {
+      assert.that(redirect('http', 'thenativeweb.io:4000', '/foo/bar', {
+        hostname: 'www.thenativeweb.io',
+        protocol: 'http',
+        type: 'temporary'
+      })).is.equalTo({
+        type: 'temporary',
+        url: 'http://www.thenativeweb.io:4000/foo/bar'
+      });
+      done();
+    });
+
+    test('for another domain with the another protocol.', function (done) {
+      assert.that(redirect('http', 'thenativeweb.io:4000', '/foo/bar', {
+        hostname: 'www.thenativeweb.io',
+        protocol: 'https',
+        type: 'temporary'
+      })).is.equalTo({
+        type: 'temporary',
+        url: 'https://www.thenativeweb.io:4000/foo/bar'
+      });
+      done();
+    });
+
+    test('for another domain without changing protocol.', function (done) {
+      assert.that(redirect('https', 'thenativeweb.io:4000', '/foo/bar', {
+        hostname: 'www.thenativeweb.io',
+        type: 'temporary'
+      })).is.equalTo({
+        type: 'temporary',
+        url: 'https://www.thenativeweb.io:4000/foo/bar'
       });
       done();
     });
