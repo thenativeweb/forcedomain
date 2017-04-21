11bc0e7abf0f24a316b89cfe04257278f2f4aeac
Golo Roden <golo.roden@thenativeweb.io>
2017-04-21 09:05:21 +0200

    Release 0.8.1.

---
+++ dist/redirect.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var _ = require('lodash');
+
+var rewrite = require('./rewrite');
+
+var redirect = function redirect(protocol, hostHeader, url, options) {
+  options = _.defaults(options, {
+    protocol: undefined,
+    type: 'permanent'
+  });
+
+  var hostHeaderParts = (hostHeader || '').split(':');
+  var hostname = hostHeaderParts[0] || '';
+  var port = hostHeaderParts[1] - 0 || undefined;
+  var targetProtocol = options.protocol ? options.protocol : protocol;
+
+  if (hostname === 'localhost' || hostname === '127.0.0.1' || hostname.startsWith('192.168.') || hostname === options.hostname && port === options.port && protocol === targetProtocol || options.excludeRule && hostname.match(options.excludeRule)) {
+    return null;
+  }
+
+  /* eslint-disable prefer-template */
+  var route = targetProtocol + '://' + hostname + (port ? ':' + port : '') + url;
+  /* eslint-enable prefer-template */
+  var rewrittenRoute = rewrite(route, options);
+
+  return {
+    type: options.type,
+    url: rewrittenRoute
+  };
+};
+
+module.exports = redirect;
---
+++ dist/rewrite.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var url = require('url');
+
+var _ = require('lodash');
+
+var rewrite = function rewrite(route, options) {
+  options = _.defaults({
+    protocol: undefined,
+    hostname: undefined
+  }, options);
+
+  var parsedRoute = url.parse(route);
+
+  parsedRoute.host = undefined;
+
+  if (options.protocol) {
+    parsedRoute.protocol = options.protocol;
+  }
+  if (options.hostname) {
+    parsedRoute.hostname = options.hostname;
+  }
+  if (options.port) {
+    parsedRoute.port = options.port;
+  }
+
+  return url.format(parsedRoute);
+};
+
+module.exports = rewrite;
