4f12c28112179654d155fdd4f51cdd8350d9fbd0
Golo Roden <golo.roden@thenativeweb.io>
2018-03-19 08:55:01 +0100

    Update dependencies.

--- roboter.js
+++ roboter.js
@@ -31,7 +31,10 @@ roboter.
         // Combined licenses
         '(Apache-2.0 OR MPL-1.1)',
         'BSD-3-Clause OR MIT',
+        'CC0-1.0',
+        'CC-BY-3.0',
         '(MIT AND CC-BY-3.0)',
+        '(MIT OR Apache-2.0)',
         '(WTFPL OR MIT)'
       ],
 
@@ -39,14 +42,17 @@ roboter.
         // BSD-3-Clause, see https://github.com/deoxxa/duplexer2/blob/0.0.2/LICENSE.md
         duplexer2: '0.0.2',
 
+        // BSD-3-Clause, see https://github.com/estools/esquery/blob/v1.0.0/license.txt
+        esquery: '1.0.0',
+
         // MIT, see https://github.com/mklabs/node-fileset/blob/v0.2.1/LICENSE-MIT
         fileset: '0.2.1',
 
         // MIT, https://github.com/tarruda/has/blob/1.0.1/package.json
         has: '1.0.1',
 
-        // BSD-2-Clause, see https://github.com/facebook/regenerator/blob/85c9e43331576be96e5dcc61757995397ab15b77/LICENSE
-        'regenerator-transform': '0.9.11',
+        // BSD-2-Clause, see https://github.com/facebook/regenerator/blob/30d34536b9e3f7a2873b04a16ec66fec9c8246f6/LICENSE
+        'regenerator-transform': '0.10.1',
 
         // BSD-2-Clause, see https://github.com/jviereck/regjsparser/blob/0.1.5/LICENSE.BSD
         regjsparser: '0.1.5',
